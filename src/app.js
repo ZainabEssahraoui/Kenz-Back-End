b458efb4ecc82833afce5093092c2033ce771abb
ZainabEssahraoui <ZainabEssahraoui@github.com>
2025-12-28 19:08:35 +0100

    Normalize Express version and project structure

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const opportuniteRoutes = require("../routes/opportunite.routes");
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(' SERVER WORKS');
-});
-app.use(express.urlencoded({ extended: true })); // optionnel mais recommandé
+app.use(cors());
 app.use(express.json());
+
+// test route
+app.get('/', (req, res) => {
+  res.send('SERVER WORKS');
+});
+
+// routes
 app.use("/api/opportunites", opportuniteRoutes);
 
 module.exports = app;
