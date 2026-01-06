dae7a1716628d608be903d9e07ada5bd123b0ada
ZainabEssahraoui <zainabzrigue@gmail.com>
2026-01-06 17:05:08 +0100

    Refactor FormationSchema with new fields and defaults
    Updated frais to have a default value and added mode_apprentissage and statut_financier fields with enums.

--- models/Formation.js
+++ models/Formation.js
@@ -4,8 +4,20 @@ const mongoose = require("mongoose");
 
 const FormationSchema = new mongoose.Schema({
   etablissement: String,
-  frais: Number,
+  frais: {
+    type: Number,
+    default: 0
+  },
+  mode_apprentissage: {
+    type: String,
+    enum: [" distance", "presentiel"],
+  },
+  statut_financier: {
+    type: String,
+    enum: ["free", "paid"],
+  },
   duree: String
+  
 });
 
 module.exports = Opportunite.discriminator("Formation", FormationSchema);
