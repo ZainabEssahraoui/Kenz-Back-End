d9e1e1f6710e0b2e2434ccb8c6f6a7d41af93814
Nadia-rabih <rabihnadia337@gmail.com>
2026-01-07 14:04:19 +0100

    Update commentaire.routes.js
    second commit

--- routes/commentaire.routes.js
+++ routes/commentaire.routes.js
@@ -1,13 +1,15 @@
+// routes/commentaire.routes.js
+
 const express = require("express");
 const router = express.Router();
-const Commentaire = require("../models/Commentaire.routes.js");
+const Commentaire = require("../models/commentaire.js");
 
 // Ajouter un commentaire
 router.post("/", async (req, res) => {
   try {
-    const commentaire = new Commentaire(req.body);
-    await commentaire.save();
-    res.status(201).json(commentaire);
+    const nouveauCommentaire = new Commentaire(req.body);
+    const commentaireSauve = await nouveauCommentaire.save();
+    res.status(201).json(commentaireSauve);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
@@ -19,7 +21,7 @@ router.get("/opportunite/:id", async (req, res) => {
     const commentaires = await Commentaire.find({ opportunite: req.params.id })
       .populate("utilisateur", "firstName lastName email")
       .populate("opportunite", "titre");
-    res.json(commentaires);
+    res.status(200).json(commentaires);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -31,7 +33,7 @@ router.get("/utilisateur/:id", async (req, res) => {
     const commentaires = await Commentaire.find({
       utilisateur: req.params.id,
     }).populate("opportunite", "titre description");
-    res.json(commentaires);
+    res.status(200).json(commentaires);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
